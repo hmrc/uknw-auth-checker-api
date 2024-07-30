8fbb384e149b30c0b4a91e7570cd5246b1fd8391
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-07-30 14:19:51 +0100

    NP-244: Added CorrelationIdGenerator trait and optimised appConfig

--- app/uk/gov/hmrc/uknwauthcheckerapi/connectors/BaseConnector.scala
+++ app/uk/gov/hmrc/uknwauthcheckerapi/connectors/BaseConnector.scala
@@ -26,8 +26,9 @@ import play.api.libs.json.{JsResult, Reads}
 import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.client.RequestBuilder
 import uk.gov.hmrc.http.{HttpResponse, Retries, UpstreamErrorResponse}
+import uk.gov.hmrc.uknwauthcheckerapi.utils.CorrelationIdGenerator
 
-trait BaseConnector extends Retries {
+trait BaseConnector extends Retries with CorrelationIdGenerator {
 
   override def actorSystem:   ActorSystem
   override def configuration: Config
